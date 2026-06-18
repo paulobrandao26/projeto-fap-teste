a08b1a5933b3b7c162758ed4e67bbbd0c9518c51
paulo <emanuellarebeka@gmail.com>
2026-06-18 10:40:13 -0300

    Enviando meus arquivos do projeto

--- git.py
+++ git.py
@@ -1,6 +1,4 @@
 nome = input("digite o seu nome:")
-print (
-    f"ola " + nome + "! bem-vindo ao python"
-)
+print (f"ola {nome}! bem-vindo ao python")
 idade = int(input("digite a sua idade: "))
 print (f"Você tem {idade} anos.")
